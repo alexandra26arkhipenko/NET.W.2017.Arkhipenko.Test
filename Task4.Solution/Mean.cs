9add64e6c4b75a15f7be055621beb92b32b28f9b
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-11-28 10:45:35 +0300

    Task 4

---
+++ Task4.Solution/Mean.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4.Solution
+{
+    public class Mean : IPredicate
+    {
+        public double AveragingMethod(List<double> values)
+        {
+            return values.Sum()/ values.Count;
+        }
+    }
+}
