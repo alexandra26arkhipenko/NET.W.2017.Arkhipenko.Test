68871338021e145d6e34bf97c169a3f3d9731dee
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-11-28 21:43:35 +0300

    Some Changes

--- Task4.Solution/Calculator.cs
+++ Task4.Solution/Calculator.cs
@@ -5,8 +5,6 @@ namespace Task4.Solution
 {
     public class Calculator
     {
-
-
         public double CalculateAverage(List<double> values, IAverageInterface averagingMethod)
         {
             if (values == null)
